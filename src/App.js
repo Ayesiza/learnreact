658bae99c1cad419f337241099cb11c3c985950e
Ayesiza <yesiha2001@gmail.com>
2021-02-19 22:21:33 +0300

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment} from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
-// import axios from 'axios';
+
 import Register from './component/Register'
 import Login from './component/Login'
 import Navbar from './component/Navbar'
 import Home from './component/Home'
 import Shop from './component/Shop';
-// import ImageLightBox from './component/ImageLightBox'
+
 
 function App() {
   return(
